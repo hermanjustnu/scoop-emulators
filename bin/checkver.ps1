8219a0988a19a5076edba7fb210e048dadbb66f1
Mathias Hermansson <herman@skenbe.net>
2017-07-27 00:50:42 +0200

    Update checkver.ps1

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,5 +1,4 @@
-#$bin = split-path (scoop which scoop)
-$bin = "D:\Dev\Projects\scoop\bin"
+$bin = split-path (scoop which scoop)
 $checkver = "$bin\checkver.ps1"
 $dir = "$psscriptroot\.." # checks the parent dir
 iex -command "$checkver -dir $dir $($args |% { "$_ " })"
