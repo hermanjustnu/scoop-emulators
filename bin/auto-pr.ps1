d3ed2ed099e459e08eda9411148ffaae7dbfc2c1
beyondmeat <51850644+beyondmeat@users.noreply.github.com>
2022-03-07 00:36:44 -0700

    update

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "borger/scoop-emulators:master"
+    [String]$upstream = "hermanjustnu/scoop-emulators:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
